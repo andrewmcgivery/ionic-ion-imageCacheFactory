28aa0a5660c623657bc14eacff603e4568931fc5
andrewmcgivery <andrew@mcgivery.com>
2015-03-15 10:53:25 -0400

    Added module

---
+++ ionic.ion.imagecachefactory.js
@@ -0,0 +1,28 @@
+angular.module('ionic.ion.imagecachefactory', ['ionic'])
+
+.factory("$ImageCacheFactory", function($q,$timeout){
+	return {
+		Cache: function(urls){
+			var promises = [];
+			for(i=0;i<urls.length;i++){
+				var deferred = $q.defer();
+				var img = new Image();
+				img.onload = (function(deferred){
+					return function(){
+						deferred.resolve();
+					}
+				})(deferred);
+				
+				img.onerror = (function(deferred,url){
+					return function(){
+						deferred.reject(url);
+					}
+				})(deferred,urls[i]);
+
+				promises.push(deferred.promise);
+				img.src = urls[i];
+			}
+			return $q.all(promises);
+		}
+	}
+});
