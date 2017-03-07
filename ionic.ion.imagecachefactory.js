ddf585341c03533e81470eea78a6523f97f71ab2
Rafael Siqueira <rafael@rsiqueira.local>
2017-03-07 00:29:37 -0300

    Add feature onError Ignore to avoid rejection if some images were not found

--- ionic.ion.imagecachefactory.js
+++ ionic.ion.imagecachefactory.js
@@ -2,9 +2,12 @@ angular.module('ionic.ion.imageCacheFactory', [])
 
 .factory('$ImageCacheFactory', ['$q', function($q) {
     return {
-        Cache: function(urls) {
+        Cache: function(urls, onErrorIgnore) {
             if (!(urls instanceof Array))
                 return $q.reject('Input is not an array');
+
+            if (typeof onErrorIgnore === 'undefined')
+                onErrorIgnore = false;
 
             var promises = [];
             
@@ -17,12 +20,14 @@ angular.module('ionic.ion.imageCacheFactory', [])
                         deferred.resolve();
                     }
                 })(deferred);
-                
-                img.onerror = (function(deferred,url) {
-                    return function(){
-                        deferred.reject(url);
-                    }
-                })(deferred,urls[i]);
+
+                if (onErrorIgnore) {                
+                    img.onerror = (function(deferred,url) {
+                        return function(){
+                            deferred.reject(url);
+                        }
+                    })(deferred,urls[i]);
+                }
 
                 promises.push(deferred.promise);
                 img.src = urls[i];
